2ce32df7f0e74a7b826b58acdb3255b32fd14d5e
Santiago Bartesaghi <santib@hey.com>
2021-12-29 19:25:13 -0300

    Initial commit

---
+++ lib/chaotic_order.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "chaotic_order/version"
+
+module ChaoticOrder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/chaotic_order_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ChaoticOrder do
+  it "has a version number" do
+    expect(ChaoticOrder::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
